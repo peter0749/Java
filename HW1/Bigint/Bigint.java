c54a8c3e10f143bb7dfcc77baa73d2ac94bac1a2
peterMac <foo@bar.deadbeef>
2017-04-07 14:38:09 +0800

    foo

--- HW1/Bigint/Bigint.java
+++ HW1/Bigint/Bigint.java
@@ -2,13 +2,12 @@ class Bigint {
     // private:
     private java.util.ArrayList<Integer> data; // Initialize in constructor
     private int dataLength;
-    private boolean sign;
     private void regulate () {
         int i=0;
         for (i=this.data.size()-1; i>=0 && this.data.get(i)==0; --i);
         this.dataLength = i+1;
     }
-    void strToBigint(String str) {
+    private void strToBigint(String str) {
         char [] charArr = str.toCharArray();
         int strLen = str.length();
         for (int i=0, j=strLen-1; j>=0; ++i, --j) {
@@ -17,9 +16,21 @@ class Bigint {
         this.dataLength = strLen;
         this.regulate();
     }
+    private int _cmp(Bigint ext) {
+        if ( this.dataLength > ext.dataLength ) return 1; // this > ext
+        else if (this.dataLength < ext.dataLength ) return -1; // this < ext
+        // same degree
+        for (int i=this.dataLength-1; i>=0; --i) {
+            if ( this.data.get(i) > ext.data.get(i) ) return 1; // this > ext
+            else if ( this.data.get(i) < ext.data.get(i) ) return -1; // this < ext
+            // else same
+        }
+        return 0; // same
+    }
 
     // public:
     public String toString() {
+        if (this.dataLength==0) return "0";
         String str="";
         for (int i=this.dataLength-1; i>=0; --i) {
             str = str + this.data.get(i);
@@ -36,6 +47,24 @@ class Bigint {
         return val;
         // not implement yet
     }
+    public boolean isGreaterThan (Bigint ext) {
+        return this._cmp(ext) > 0;
+    }
+    public boolean isLessThan (Bigint ext) {
+        return this._cmp(ext) < 0;
+    }
+    public boolean isEqualTo (Bigint ext) {
+        return this._cmp(ext) == 0;
+    }
+    public boolean isNotEqualTo (Bigint ext) {
+        return this._cmp(ext) != 0;
+    }
+    public boolean isGreaterThanOrEqulTo (Bigint ext) {
+        return this._cmp(ext) >= 0;
+    }
+    public boolean isLessThanOrEqulTo (Bigint ext) {
+        return this._cmp(ext) <= 0;
+    }
     public Bigint Add (Bigint ext) {
         // not implement yet
         int maxLen = Math.max(ext.dataLength,this.dataLength);
@@ -51,17 +80,51 @@ class Bigint {
         }
         return new Bigint(temp);
     }
-    /*
-    public Bigint Sub (Bigint ext) {
+    public Bigint Diff (Bigint ext) {
         // not implement yet
+        Bigint ref1=null, ref2=null;
+        if (this.isLessThan(ext)) {
+            ref1 = ext;
+            ref2 = this;
+        } else {
+            ref1 = this;
+            ref2 = ext;
+        }
+        int maxLen = ref1.dataLength;
+        java.util.ArrayList<Integer> temp = new java.util.ArrayList<Integer> (maxLen+2);
+        for (int t=maxLen+1; t>=0; --t) temp.add(0); // initialize maxLen elemnts
+        for (int i=0; i<maxLen; ++i) {
+            temp.set(i, (i<ref1.dataLength?ref1.data.get(i):0) - (i<ref2.dataLength?ref2.data.get(i):0));
+        }
+        for (int i=0; i<maxLen-1; ++i) {
+            if ( temp.get(i) < 0 ) {
+                temp.set(i+1, temp.get(i+1)-1);
+                temp.set(i, temp.get(i)+10 );
+            }
+        }
+        return new Bigint(temp);
     }
+
     public Bigint Mul (Bigint ext) {
-        // not implement yet
+        int maxLen = this.dataLength + ext.dataLength;
+        java.util.ArrayList<Integer> temp = new java.util.ArrayList<Integer>(maxLen+2);
+        for (int i=maxLen; i>=0; --i) temp.add(0);
+        for (int i=0; i<this.dataLength; ++i) {
+            for (int j=0; j<ext.dataLength; ++j) {
+                temp.set ( i+j, temp.get(i+j) + this.data.get(i) * ext.data.get(j));
+            }
+        }
+        for (int i=0; i<maxLen; ++i) {
+            temp.set ( i+1, temp.get(i+1)+temp.get(i)/10);
+            temp.set ( i  , temp.get(i)%10 );
+        }
+        return new Bigint(temp);
     }
-    public Bigint Div (Bigint ext) {
-        // not implement yet
-    }
-    */
+    /*
+       public Bigint Div (Bigint ext) {
+// not implement yet
+       }
+       */
 
     // constructor:
     Bigint(String str) {
--- HW1/Bigint/Main.java
+++ HW1/Bigint/Main.java
@@ -1,13 +1,24 @@
 public class Main {
     public static void main(String args[]) {
         if (args.length != 2) {
-            System.out.println("Please enter two integer!");
+            System.out.println("Please enter exactly two integer!");
             return ;
         }
         Bigint a = new Bigint(args[0]);
         Bigint b = new Bigint(args[1]);
+        System.out.println ("Note:\nOnly implements positive integers");
         System.out.println ("First Bigint:\t"+a);
         System.out.println ("Second Bigint:\t"+b);
-        System.out.println ("First + Second:\t"+a.Add(b));
+        System.out.println ("First double:\t"+a.toDouble());
+        System.out.println ("Second double:\t"+b.toDouble());
+        System.out.println (a+" < "+b+" : "+a.isLessThan(b));
+        System.out.println (a+" = "+b+" : "+a.isEqualTo(b));
+        System.out.println (a+" > "+b+" : "+a.isGreaterThan(b));
+        System.out.println (a+" <= "+b+" : "+a.isLessThanOrEqulTo(b));
+        System.out.println (a+" >= "+b+" : "+a.isGreaterThanOrEqulTo(b));
+        System.out.println (a+" != "+b+" : "+a.isNotEqualTo(b));
+        System.out.println (a+" + "+b+" = "+a.Add(b));
+        System.out.println ("| "+a+" - "+b+" | = "+a.Diff(b));
+        System.out.println (a+" * "+b+" = "+a.Mul(b));
     }
 };
