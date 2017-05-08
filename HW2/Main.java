87d004c73682b11d790c91ecf374ca823f645321
peterMac <foo@bar.deadbeef>
2017-05-09 07:14:06 +0800

    foo

--- HW2/Main.java
+++ HW2/Main.java
@@ -1,21 +1,23 @@
 public class Main {
     public static void main(String[] args) {
         Circle c = new Circle(3);
-        System.out.println(c+" "+c.getArea());
+        System.out.println("Radius of circle: "+c+"\nArea: "+c.getArea());
         Square s = new Square(3,10);
-        System.out.println(s+" "+s.getArea());
+        System.out.println("Shape of square: "+s+"\nArea: "+s.getArea());
         Triangle t=  new Triangle(3,4,5);
-        System.out.println(t+" "+t.getArea());
+        System.out.println("Shape of triangle: "+t+"\nArea: "+t.getArea());
         Sphere sph = new Sphere(10);
-        System.out.println(sph+" "+sph.getArea()+" "+sph.getVolume());
+        System.out.println("Shape of sphere: "+sph+"\nArea: "+sph.getArea()+"\nVolume: "+sph.getVolume());
         Cube cube = new Cube(3,4,5);
-        System.out.println(cube+" "+cube.getArea()+" "+cube.getVolume());
-        Tetrahedron tet = new Tetrahedron(0,0,0,
-                                          1,1,0,
-                                          0,1,1,
-                                          1,0,1);
-        System.out.println(tet);
-        System.out.println(tet.getArea());
-        System.out.println(tet.getVolume());
+        System.out.println("Shape of cube: "+cube+"\nArea: "+cube.getArea()+"\nVolume: "+cube.getVolume());
+        Tetrahedron tet = new Tetrahedron(
+                                            0,0,0,
+                                            1,1,0,
+                                            0,1,1,
+                                            1,0,1
+                                         );
+        System.out.println("Vertics of tetrahedron: "+tet);
+        System.out.println("Area: "+tet.getArea());
+        System.out.println("Volume: "+tet.getVolume());
     }
 }
