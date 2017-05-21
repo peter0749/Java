c90301298d5027bc708770fd9e518ea1ffdbb77c
peterMac <foo@bar.deadbeef>
2017-05-21 19:49:38 +0800

    foo

--- HW3/PanelLayout.java
+++ HW3/PanelLayout.java
@@ -1,6 +1,8 @@
 import java.awt.Color;
 import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.GridLayout;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
@@ -10,8 +12,10 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
 import java.util.Map;
 import java.util.HashMap;
+import javax.swing.JOptionPane;
 
 public class PanelLayout extends JFrame implements KeyListener
 {
@@ -32,13 +36,15 @@ public class PanelLayout extends JFrame implements KeyListener
     private final JTextArea typeIn;
     private final JPanel[] buttonPanels;
     private final JPanel mainPanel;
-    private final JButton getAccuracy;
+    private final JLabel getAccuracy;
     private final JButton[] buttons;
     private final String[] buttonName;
     private final int[] buttonLevel;
     private final Map<Integer,Integer> revKeyCodes;
     private final int[] keyCodes;
     private final Color[] oldColor;
+    private String inputText;
+    private boolean capital;
 
     public PanelLayout() {
         super("Type Tutor");
@@ -46,30 +52,33 @@ public class PanelLayout extends JFrame implements KeyListener
         final int panelNum  = 8;
         // Initialize buttonName:
 
+        inputText = new String("");
+        capital = false;
+
         buttonName = new String[]{
             "~", "1", "2", "3",
-            "4", "5", "6", "7",
-            "8", "9", "0", "-",
-            "=+", "Backspace", "Tab", "Q",
-            "W", "E", "R", "T",
-            "Y", "U", "I", "O",
-            "P", "[", "]", "\\",
-            "Caps", "A", "S", "D",
-            "F", "G", "H", "J",
-            "K", "L", ";:", "\'\"",
-            "Enter", "Shift", "Z", "X",
-            "C", "V", "B", "N",
-            "M", ",", ".", "/?",
-            "Space", "↑", "←", "↓", "→"
+                "4", "5", "6", "7",
+                "8", "9", "0", "-",
+                "=+", "Backspace", "Tab", "Q",
+                "W", "E", "R", "T",
+                "Y", "U", "I", "O",
+                "P", "[", "]", "\\",
+                "Caps", "A", "S", "D",
+                "F", "G", "H", "J",
+                "K", "L", ";:", "\'\"",
+                "Enter", "Shift", "Z", "X",
+                "C", "V", "B", "N",
+                "M", ",", ".", "/?",
+                "Space", "↑", "←", "↓", "→"
         };
 
         keyCodes = new int[]{
             192, 49, 50, 51, 52, 53, 54, 55, 56, 57,
-            48 , 45, 61, 8 , 9 , 81, 87, 69, 82, 84,
-            89 , 85, 73, 79, 80, 91, 93, 92, 20, 65,
-            83 , 68, 70, 71, 72, 74, 75, 76, 59, 222,
-            10 , 16, 90, 88, 67, 86, 66, 78, 77, 44,
-            46 , 47, 32, 38, 37, 40, 39
+                48 , 45, 61, 8 , 9 , 81, 87, 69, 82, 84,
+                89 , 85, 73, 79, 80, 91, 93, 92, 20, 65,
+                83 , 68, 70, 71, 72, 74, 75, 76, 59, 222,
+                10 , 16, 90, 88, 67, 86, 66, 78, 77, 44,
+                46 , 47, 32, 38, 37, 40, 39
         };
 
         buttonLevel = new int[]{ 14, 14, 13, 11, 5 };
@@ -96,7 +105,7 @@ public class PanelLayout extends JFrame implements KeyListener
         buttonPanels[0] = new JPanel();
         buttonPanels[0].setLayout(new BorderLayout());
         buttonPanels[0].add(infoText, BorderLayout.WEST);
-        getAccuracy = new JButton("GetAccuary");
+        getAccuracy = new JLabel("Get Accuracy");
         buttonPanels[0].add(getAccuracy, BorderLayout.EAST);
         mainPanel.add(buttonPanels[0]);
         buttonPanels[1] = new JPanel();
@@ -129,7 +138,14 @@ public class PanelLayout extends JFrame implements KeyListener
         Integer butt_idx = revKeyCodes.get(event.getKeyCode());
         if (butt_idx==null) return;
         oldColor[butt_idx] = buttons[butt_idx].getBackground();
-        buttons[butt_idx].setBackground(Color.BLUE);
+        if (event.getKeyCode()==KeyEvent.VK_CAPS_LOCK || event.getKeyCode()==KeyEvent.VK_SHIFT) {
+            buttons[butt_idx].setBackground(Color.BLUE);
+            capital=!capital;
+        } else if (textProcess(event)) {
+            buttons[butt_idx].setBackground(Color.BLUE);
+        } else {
+            buttons[butt_idx].setBackground(Color.RED);
+        }
         buttons[butt_idx].setOpaque(true);
     }
 
@@ -137,12 +153,69 @@ public class PanelLayout extends JFrame implements KeyListener
     public void keyReleased(KeyEvent event) {
         Integer butt_idx = revKeyCodes.get(event.getKeyCode());
         if (butt_idx==null) return;
+        if (event.getKeyCode()==KeyEvent.VK_SHIFT) capital=!capital;
         buttons[butt_idx].setBackground(oldColor[butt_idx]);
         buttons[butt_idx].setOpaque(true);
     }
 
     @Override
     public void keyTyped(KeyEvent event) {
+        String acc = computeAccuracy();
+        getAccuracy.setText("<html>Accuracy:<br/>"+acc+"</html>");
         // do nothing
+    }
+
+    private String simpleEncoder(char ch) {
+        if (ch==' ') return new String("&nbsp;");
+        if (ch=='\n') return new String("<br/>");
+        if (ch=='\t') return new String("&nbsp;&nbsp;&nbsp;&nbsp;");
+        return new String(""+ch);
+    }
+    private boolean textProcess(KeyEvent event) {
+        String tempStr;
+        switch (event.getKeyCode()) {
+            case KeyEvent.VK_BACK_SPACE:
+                if (inputText.length()>0)
+                    inputText = inputText.substring(0, inputText.length()-1);
+                break;
+            case KeyEvent.VK_TAB:
+                inputText += "\t";
+                break;
+            case KeyEvent.VK_ENTER:
+                inputText += "\n";
+                break;
+            default:
+                tempStr = new String(""+event.getKeyChar());
+                inputText += (capital?tempStr.toUpperCase():tempStr.toLowerCase());
+        }
+        tempStr = new String("");
+        for (int i=0; i<context.length(); ++i) {
+            if(i<inputText.length()) {
+                if (inputText.charAt(i)!=context.charAt(i)) {
+                    tempStr += "<font color=\"red\">";
+                    tempStr += simpleEncoder(context.charAt(i));
+                    tempStr += "</font>";
+                } else {
+                    tempStr += "<font color=\"green\">";
+                    tempStr += simpleEncoder(context.charAt(i));
+                    tempStr += "</font>";
+                }
+            } else {
+                tempStr += simpleEncoder(context.charAt(i));
+            }
+        }
+        typeIn.setText(inputText);
+        sampleText.setText("<html>"+tempStr+"</html>");
+        return !(inputText.length() > context.length() || 
+                (inputText.length()>0 &&
+                 inputText.charAt(inputText.length()-1)!=
+                 context.charAt(inputText.length()-1) ) );
+    }
+    private String computeAccuracy() {
+        int wrong=0;
+        for (int i=0; i<context.length(); ++i)
+            if (i>=inputText.length() || inputText.charAt(i)!=context.charAt(i))
+                ++wrong;
+        return String.format("%d / %d", (context.length()-wrong), context.length());
     }
 }
