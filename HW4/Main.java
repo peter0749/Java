e04581805e88494daf42ea1e6930e6cd562cf316
jengku <jengku@gmail.com>
2017-06-14 10:13:01 +0800

    foo

--- HW4/Main.java
+++ HW4/Main.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 import java.util.InputMismatchException;
 import java.util.Collections;
 import java.util.TreeSet;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -11,16 +13,20 @@ class Main
         Scanner scanner = new Scanner(System.in);
         String str = new String();
         str = scanner.nextLine();
-        str = str.toLowerCase().replaceAll("[^a-z0-9 ]","").replaceAll(" +", " ");
-        ArrayList<String> strArr = new ArrayList<String>(Arrays.asList(str.split(" ")));
-        TreeSet<String> strSet = new TreeSet<String>(strArr);
+        ArrayList<String> strArr = new ArrayList<String>(Arrays.asList(str.toLowerCase().replaceAll("[^a-z0-9 ]","").replaceAll(" +", " ").split(" ")));
+        TreeSet<String> strSet = new TreeSet<String>(strArr); // For ordering, part of homework (so I don't use TreeMap)
+        HashMap<String,Integer> strTable = new HashMap<String,Integer>(); // For counting
+        for (String v: strArr) {
+            Integer count = strTable.get(v);
+            strTable.put(v, (count==null?0:count)+1); // Counting. If null, return 0
+        }
         System.out.println("Ascending order:");
         for (String v: strSet) {
-            System.out.println(v + " appears " + Collections.frequency(strArr, v) + " times");
+            System.out.println(v + " appears " + strTable.get(v) + " times");
         }
         System.out.println("\nDescending order:");
         for (String v: strSet.descendingSet()) {
-            System.out.println(v + " appears " + Collections.frequency(strArr, v) + " times");
+            System.out.println(v + " appears " + strTable.get(v) + " times");
         }
     }
 };
